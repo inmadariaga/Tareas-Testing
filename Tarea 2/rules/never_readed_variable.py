dc5b152499e8a60614a12f8ceb924daf2c267255
Ignacio Madariaga <inmadariaga@uc.cl>
2022-08-30 23:09:28 -0400

    refactor and debuged never readed variable

--- Tarea 2/rules/never_readed_variable.py
+++ Tarea 2/rules/never_readed_variable.py
@@ -1,52 +1,81 @@
 from . rule import *
 
-class NeverReadedVariableVisitor(WarningNodeVisitor):
+class VariableCounterVisitor(NodeVisitor):
     def __init__(self):
-        super().__init__()
-        self.unused_variables = []
+        self.variables = []
+        self.args = []
+        self.names = []
 
-    def visit_Return(self, node: Return):
-        unused_variables = [var.targets[0].id for var in self.unused_variables]
-        if isinstance(node.value, Name):
-            if node.value.id in unused_variables:
-                index = unused_variables.index(node.value.id)
-                unused_variables.pop(index)
-                self.unused_variables.pop(index)
+    def add_name(self, name):
+        if len(self.names) == 0 and isinstance(name, Name):
+            self.names.append(name)
+        elif name.id not in [name.id for name in self.names]:
+            self.names.append(name)
 
-    def visit_BinOp(self, node: BinOp):
-        unused_variables = [var.targets[0].id for var in self.unused_variables]
-        if isinstance(node.left, Name):
-            if node.left.id in unused_variables:
-                index = unused_variables.index(node.left.id)
-                unused_variables.pop(index)
-                self.unused_variables.pop(index)
+    def check_binop(self, node):
+        if isinstance(node.left, BinOp):
+            self.check_binop(node.left)
+        elif isinstance(node.left, Name):
+            self.add_name(node.left)
         if isinstance(node.right, Name):
-            if node.right.id in unused_variables:
-                index = unused_variables.index(node.right.id)
-                unused_variables.pop(index)
-                self.unused_variables.pop(index)
-        NodeVisitor.generic_visit(self, node)
+            self.add_name(node.right)
 
-    def visit_Call(self, node: Call):
-        unused_variables = [var.targets[0].id for var in self.unused_variables]
-        for var in node.args:
-            if var.id in unused_variables:
-                index = unused_variables.index(var.id)
-                unused_variables.pop(index)
-                self.unused_variables.pop(index)
-        NodeVisitor.generic_visit(self, node)
+    def check_call(self, node):
+        for arg in node.args:
+            if isinstance(arg, Name):
+                self.add_name(arg)
 
-    def visit_Assign(self, node: Assign):
-        self.unused_variables.append(node)
-        NodeVisitor.generic_visit(self, node)
+    def check_assign(self, node):
+        if isinstance(node.value, BinOp):
+            self.check_binop(node.value)
+        elif isinstance(node.value, Name):
+            self.add_name(node.value)
+        elif isinstance(node.value, Call):
+            self.check_call(node.value)
 
+    def check_return(self, node):
+        if isinstance(node.value, BinOp):
+            self.check_binop(node.value)
+        elif isinstance(node.value, Call):
+            self.check_call(node.value)
+        elif isinstance(node.value, name):
+            self.add_name(node.value)
+
+    def visit_FunctionDef(self, node:FunctionDef):
+        if len(node.args.args) != 0:
+            for arg in node.args.args:
+                self.args.append(arg)
+        for element in node.body:
+            if isinstance(element, Assign):
+                if element.targets[0].id not in [var.targets[0].id for var in self.variables]:
+                    self.variables.append(element)
+        for element in node.body:
+            if isinstance(element, Assign):
+                self.check_assign(element)
+            if isinstance(element, Return):
+                self.check_return(element)
+        variables = [var.targets[0].id for var in self.variables]
+        args = [arg.arg for arg in self.args]
+        for name in self.names:
+            if name.id in variables:
+                index = variables.index(name.id)
+                variables.pop(index)
+                self.variables.pop(index)
+            if name.id in args:
+                index = args.index(name.id)
+                args.pop(index)
+                self.args.pop(index)
+
+class NeverReadedVariableVisitor(WarningNodeVisitor):
     def visit_FunctionDef(self, node: FunctionDef):
-        NodeVisitor.generic_visit(self, node)
-        if len(self.unused_variables) != 0:
-            for unused_variable in self.unused_variables:
-                if isinstance(unused_variable, Name):
-                    self.addWarning("NeverReadedVariable", unused_variable.lineno, 'variable ' + unused_variable.targets[0].id + ' is never readed')
-            self.unused_variables = []
+        visitor = VariableCounterVisitor()
+        visitor.visit(node)
+        if len(visitor.args) != 0:
+            for arg in visitor.args:
+                self.addWarning('NeverReadedVariable', arg.lineno, 'argument ' + arg.arg + ' is never used')
+        if len(visitor.variables) != 0:
+            for var in visitor.variables:
+                self.addWarning('NeverReadedVariable', var.lineno, 'variable ' + var.targets[0].id + ' is defined and never used')
 
 
 class NeverReadedVariableRule(Rule):
