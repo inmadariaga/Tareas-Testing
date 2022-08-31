ac1577db3f3c4496776619a95be20320ed5a8b5b
Ignacio Madariaga <inmadariaga@uc.cl>
2022-08-31 00:27:23 -0400

    added T2 README and analyze.py final

--- Tarea 2/analyze.py
+++ Tarea 2/analyze.py
@@ -1,14 +1,18 @@
 from ast import *
 import os
 from rules.rule import *
-from rules.eval_used import *
-from rules.uncouple_method import *
-from rules.dummy_if import *
-from rules.uninitialized_attribute import *
-from rules.suspicious_variable_name import *
-from rules.never_readed_variable import *
+#from rules.eval_used import *
+#from rules.uncouple_method import *
+#from rules.dummy_if import *
+#from rules.uninitialized_attribute import *
+#from rules.suspicious_variable_name import *
+#from rules.never_readed_variable import *
+from rules.dataclass import *
 
-path = "input-code/"
+#path = "input-code/"
+path = "DataClass/"
+#path = "NeverReaded"
+#path = "SuspiciousVariableName"
 dir_list = os.listdir(path)
  
 print("Analyzing files in '", path, "' :")
@@ -19,7 +23,6 @@ for file in dir_list:
     fileContent = open(path+file).read()
     tree = parse(fileContent)
     warnings = []
-    #print(dump(tree))
     for ruleClass in Rule.__subclasses__():    
         newRule = ruleClass()
         result = newRule.analyze(tree)
