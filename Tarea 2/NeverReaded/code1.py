66e569b1c6e4675f43c7ce61ce625a8c4a710cf0
Ignacio Madariaga <inmadariaga@uc.cl>
2022-08-30 23:24:25 -0400

    added testcases for never readed variable

---
+++ Tarea 2/NeverReaded/code1.py
@@ -0,0 +1,6 @@
+def example():
+    x = 1
+    y = 2
+    z = 3
+    z = x + y + 3 + 4
+    return x + 1
---
+++ Tarea 2/NeverReaded/code3.py
@@ -0,0 +1,9 @@
+def suma(x,y):
+    return x + y
+
+def example():
+    x = 1
+    y = 2
+    a = x
+    z = suma(x,y)
+    return suma(x, y)
